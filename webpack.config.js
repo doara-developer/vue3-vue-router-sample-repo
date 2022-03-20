eb04b231426fa1df347290166a65d5c550abf0e7
doara-developer <doaradev@gmail.com>
2022-03-20 16:17:09 +0900

    Add base code

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry:'./src/index.ts',
+  mode: 'development',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'main.js',
+  },
+  resolve:{
+    extensions: [".vue", ".js", ".ts"],
+  },
+  module:{
+    rules:[
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+        options: {
+          appendTsSuffixTo: [/\.vue$/],
+        },
+      },     
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    })
+  ],
+}
