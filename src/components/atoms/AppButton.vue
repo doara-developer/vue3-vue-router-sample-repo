8131df737ad63a6e1dc84be57bed5e4a952b9b85
doara-developer <doaradev@gmail.com>
2022-03-20 17:16:29 +0900

    Add AppButton.vue

---
+++ src/components/atoms/AppButton.vue
@@ -0,0 +1,18 @@
+<template>
+    <button @click="clickButton"><slot /></button>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+    setup(_, context) {
+        const clickButton = () => {
+            context.emit("click");
+        }
+        return {
+            clickButton
+        }
+    },
+})
+</script>
--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,20 +1,27 @@
 <template>
     <div>Home</div>
-    <button @click="clickButton">Page1</button>
+    <AppButton @click="goToPage1">Page1</AppButton>
+    <AppButton @click="goToPage2">Page2</AppButton>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { useRouter } from 'vue-router';
+import AppButton from '@src/components/atoms/AppButton.vue';
 
 
 export default defineComponent({
+    components: {AppButton},
     setup() {
         const router = useRouter()
-        const clickButton = () => {
+        const goToPage1 = () => {
             router.push("/page1");
         }
+        const goToPage2 = () => {
+            router.push("/page2");
+        }
         return {
-            clickButton
+            goToPage1,
+            goToPage2,
         }
     },
 })
